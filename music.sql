bae7b388886a5f07217ed829961633f66cd1e71f
Naomi Clyde <49344161+naomi-clyde51@users.noreply.github.com>
2026-04-13 21:02:35 -0400

    Add comments to clarify table structure in music.sql

--- music.sql
+++ music.sql
@@ -8,16 +8,16 @@ CREATE TABLE Artists (
 CREATE TABLE Albums (
   album_id INTEGER PRIMARY KEY,
   album_name TEXT NOT NULL, -- name not allowed to be NULL
-  artist_id INTEGER,
-  FOREIGN KEY (artist_id) REFERENCES Artists(artist_id)
+  artist_id INTEGER, -- artist id is whole number
+  FOREIGN KEY (artist_id) REFERENCES Artists(artist_id) -- linking artist_id in current table with album_id in Artists table
 );
 
 -- Songs table
 CREATE TABLE Songs (
   song_id INTEGER PRIMARY KEY,
   song_name TEXT NOT NULL, -- name not allowed to be NULL
-  album_id INTEGER,
-  track_number INTEGER,
-  duration_seconds INTEGER,
-  FOREIGN KEY (album_id) REFERENCES Artists(album_id)
+  album_id INTEGER, -- the associated album this song appears on
+  track_number INTEGER, -- track number of the song
+  duration_seconds INTEGER, -- how long the song is (in seconds)
+  FOREIGN KEY (album_id) REFERENCES Artists(album_id) -- linking album_id in current table with album_id in Artists table
 );
