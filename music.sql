7284842816c61daa899997c6e1679e1fdca899f1
Naomi Clyde <49344161+naomi-clyde51@users.noreply.github.com>
2026-04-13 20:43:44 -0400

    Create tables for Artists, Albums, and Songs

--- music.sql
+++ music.sql
@@ -1,1 +1,23 @@
+-- Artists table
+CREATE TABLE Artists (
+  artist_id INTEGER PRIMARY KEY,
+  artist_name TEXT NOT NULL -- name not allowed to be NULL
+);
 
+-- Albums table
+CREATE TABLE Albums (
+  album_id INTEGER PRIMARY KEY,
+  album_name TEXT NOT NULL, -- name not allowed to be NULL
+  artist_id INTEGER,
+  FOREIGN KEY (artist_id) REFERENCES Artists(artist_id)
+);
+
+-- Songs table
+CREATE TABLE Songs (
+  song_id INTEGER PRIMARY KEY,
+  song_name TEXT NOT NULL, -- name not allowed to be NULL
+  album_id INTEGER,
+  track_number INTEGER,
+  duration_seconds INTEGER,
+  FOREIGN KEY (album_id) REFERENCES Artists(album_id)
+);
